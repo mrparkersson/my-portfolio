ced622a3fc44364e1845313c755737ca43e4612e
mrparkersson <amoparkerprince78@gmail.com>
2021-12-13 19:49:44 +0000

    Add window innerwidth

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.querySelector(".union").addEventListener("click", () => {
   document.querySelector(".hamburger").style.display = "flex";
-  document.body.style.overflow = "hidden";
+  // document.body.style.overflow = "hidden";
 });
 
 document.querySelector("#xicon").addEventListener("click", () => {
   document.querySelector(".hamburger").style.display = "none";
-  document.body.style.overflow = "auto";
+  // document.body.style.overflow = "hidden";
 });
+
+document.querySelectorAll(".hamburger a").forEach((e) => {
+  e.addEventListener("click", () => {
+    document.querySelector(".hamburger").style.display = "none";
+  });
+});
+
+window.onresize = () => {
+  if (window.innerWidth > 992) {
+    document.querySelector(".hamburger").style.display = "none";
+  }
+};
