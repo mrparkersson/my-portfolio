4480beced2add1260600f87f9ac113ce36b3be04
mrparkersson <amoparkerprince78@gmail.com>
2021-12-15 08:01:11 +0000

    Add popup window

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.querySelector('.union').addEventListener('click', () => {
-  document.querySelector('.hamburger').style.display = 'flex';
+document.querySelector(".union").addEventListener("click", () => {
+  document.querySelector(".hamburger").style.display = "flex";
 });
 
-document.querySelector('#xicon').addEventListener('click', () => {
-  document.querySelector('.hamburger').style.display = 'none';
+document.querySelector("#xicon").addEventListener("click", () => {
+  document.querySelector(".hamburger").style.display = "none";
 });
 
-document.querySelectorAll('.hamburger a').forEach((e) => {
-  e.addEventListener('click', () => {
-    document.querySelector('.hamburger').style.display = 'none';
+document.querySelectorAll(".hamburger a").forEach((e) => {
+  e.addEventListener("click", () => {
+    document.querySelector(".hamburger").style.display = "none";
   });
 });
 
 window.onresize = () => {
   if (window.innerWidth > 992) {
-    document.querySelector('.hamburger').style.display = 'none';
+    document.querySelector(".hamburger").style.display = "none";
   }
 };
